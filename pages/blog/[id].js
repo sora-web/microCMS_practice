8564c100ac81ab050f8b9f7e3919f6f10a9d6637
sora-web <brave27yuto@gmail.com>
2022-08-14 16:10:42 +0900

    second commit

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,14 +1,17 @@
 import { client } from "../../libs/client";
+import styles from "../../styles/Home.module.scss";
 
 export default function BlogId({ blog }) {
   return (
-    <main>
-      <h1>{blog.title}</h1>
-      <p>{blog.publishedAt}</p>
+    <main className={styles.main}>
+      <h1 className={styles.title}>{blog.title}</h1>
+      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <p className="category">{blog.category && `${blog.category.name}`}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
         }}
+        className={styles.post}
       />
     </main>
   );
